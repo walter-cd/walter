df6f77c45540b04f3a884fbdba9dd6da3ea9d615
Jason Croucher <jace.croucher@gmail.com>
2015-08-22 11:28:00 -0700

    golint changes messenger.go
    at line 1, character 1, file messengers/messenger.gopackage comment
    should be of the form "Package messengers ..."
    at line 1, character 1, file messengers/messenger.gopackage comment
    should not have leading space
    at line 30, character 6, file messengers/messenger.goexported type
    BaseMessenger should have comment or be unexported
    at line 34, character 1, file messengers/messenger.goexported method
    BaseMessenger.Post should have comment or be unexported
    at line 34, character 1, file messengers/messenger.goreceiver name
    should be a reflection of its identity; don't use generic names such as
    "me", "this", or "self"
    at line 38, character 1, file messengers/messenger.goexported method
    BaseMessenger.Suppress should have comment or be unexported
    at line 38, character 1, file messengers/messenger.goreceiver name
    should be a reflection of its identity; don't use generic names such as
    "me", "this", or "self"
    at line 38, character 37, file messengers/messenger.godon't use
    underscores in Go names; method parameter output_type should be
    outputType

--- messengers/messenger.go
+++ messengers/messenger.go
@@ -14,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//Package messengers provides all functionality for the suported messengers
 package messengers
 
 import (
@@ -27,17 +29,20 @@ type Messenger interface {
 	Suppress(string) bool
 }
 
+//BaseMessenger struct
 type BaseMessenger struct {
 	SuppressFields []string `config:"suppress"`
 }
 
-func (self *BaseMessenger) Post(messege string) bool {
+//Post posts the supplied message
+func (baseMessenger *BaseMessenger) Post(messege string) bool {
 	return true
 }
 
-func (self *BaseMessenger) Suppress(output_type string) bool {
-	for _, suppress := range self.SuppressFields {
-		if suppress == output_type {
+//Suppress idenitifies if the supplied output type is a suppressed field
+func (baseMessenger *BaseMessenger) Suppress(outputType string) bool {
+	for _, suppress := range baseMessenger.SuppressFields {
+		if suppress == outputType {
 			return true
 		}
 	}
